04343ff498e00ad18fa18bfd7001732bb571727d
Harley Adams <hadams@microsoft.com>
2018-12-22 01:18:53 -0800

    Added xamarin app that can get fb token

---
+++ HarleyAuthXamarin/HarleyAuthXamarin/HarleyAuthXamarin.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Facebook;
+using Android.Content;
+
+namespace HarleyAuthXamarin.Droid
+{
+	[Activity(Label = "HarleyAuthXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		public static ICallbackManager CallbackManager;
+
+		protected override void OnActivityResult(
+			int requestCode,
+			Result resultCode,
+			Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			CallbackManager.OnActivityResult(requestCode, Convert.ToInt32(resultCode),
+				data);
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			// Create callback manager using CallbackManagerFactory
+			CallbackManager = CallbackManagerFactory.Create();
+
+			base.OnCreate(savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+	}
+}
---
+++ HarleyAuthXamarin/HarleyAuthXamarin/HarleyAuthXamarin/GlobalSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarleyAuthXamarin
+{
+    public static class GlobalSettings
+    {
+		public static string FacebookToken { get; set; }
+    }
+}
